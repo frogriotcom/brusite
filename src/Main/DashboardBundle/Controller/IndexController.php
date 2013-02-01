976d808cd05fe2588c786b6ec6497367fee8071a
ilyich <ilya5800@gmail.com>
2013-02-01 15:06:19 +0400

    ipoteka module

--- src/Main/DashboardBundle/Controller/IndexController.php
+++ src/Main/DashboardBundle/Controller/IndexController.php
@@ -17,28 +17,44 @@ class IndexController extends Controller
 
 		$totalAmount   = 7500000;
 		
-		$mortageAmount = 4000000;
+		$mortageAmount = 3000000;
 		$mortageTime   = 240;
 		$mortagePercent= 0.1305;
 
 		$amountPercent = 50;
 		$amountPercent = $amountPercent / 100;
 		$myAmount = $totalAmount * ($amountPercent);
-		$amountPayed = 1100000;
+		$amountPayed = 1500000;		
 		$amountToPay = $myAmount - $amountPayed;
-
 		$monthPercent = $mortagePercent / 12;
 		$countedPayment = $mortageAmount * (($monthPercent) / (1 - pow((1 + $monthPercent),( -$mortageTime))));
+		$countedPayment = $countedPayment/2;
 
-		$extraPayment = 20000;
+		$extraPayment = 0;
+		$totalAvailablePayment = 42000;
+		$extraPayment = $totalAvailablePayment - $countedPayment;
 		$realTime = 0;
 
-		for ($i = 1; $i <= $mortageTime; ++$i) {
-
-		}
+		$monthReport = array();
+		$monthReport[] = array(
+			'month'            => $realTime,
+			'amountToPay'      => $amountToPay,
+			'nextPayment'      => $countedPayment,
+			'nextExtraPayment' => $extraPayment
+		);	
 		do {
-			$amountToPay = ($amountToPay + $amountToPay * $monthPercent) - $countedPayment - $extraPayment;
+			$amountToPay = ($amountToPay + $amountToPay * $monthPercent) - ($countedPayment + $extraPayment);
 			++$realTime;
+			if ($extraPayment) {
+				$countedPayment = $amountToPay * (($monthPercent) / (1 - pow((1 + $monthPercent),( -($mortageTime-$realTime) ) )));
+				$extraPayment = $totalAvailablePayment - $countedPayment;				
+			}		
+			$monthReport[] = array(
+				'month'            => $realTime,
+				'amountToPay'      => $amountToPay,
+				'nextPayment'      => $countedPayment,
+				'nextExtraPayment' => $extraPayment
+			);	
 		} while ($amountToPay > 0);
 		
 		return $this->render('MainDashboardBundle:Index:index.html.twig', array(
@@ -52,6 +68,7 @@ class IndexController extends Controller
 			'mortageTime'    => $mortageTime,
 			'extraPayment'   => $extraPayment,
 			'realTime'       => $realTime,
+			'monthReport'    => $monthReport
 		));
 	}
 }
